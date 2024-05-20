686d47e49e09e79675688a94618c5413a7595175
alejandro <luismencos571@gmail.com>
2024-05-20 11:40:08 -0600

    arreglos de coneccion de footer y heater

--- templates/hader.php
+++ templates/hader.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -6,8 +6,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="shortcut icon" href="/images/iconocli.png" type="image/x-icon">
-    <title>Control de Comida</title>
+    <title>INGRESOS</title>
 </head>
 <body>
-<?php include_once '../../vistas/templates/navbar.php'?>
+<?php include_once '../templates/nabvar.php'?>
 <div class="container mt-5 pt-5">
